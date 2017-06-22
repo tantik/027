e35ffb9da64f458eee1883a8d04ec05c59eb70d9
Dim And <dimand81179@gmail.com>
2017-06-22 13:46:32 +0300

    Company pages

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -78,6 +78,13 @@ function scrollTo() {
 		}
 		return false;
 	});
+	$('.anchor-link').click( function(){
+		var scroll_el = $(this).attr('href');
+		if ($(scroll_el).length != 0) {
+			$('html, body').animate({ scrollTop: $(scroll_el).offset().top -20 }, 500);
+		}
+		return false;
+	});
 }
 
 /* scroll to top link */
