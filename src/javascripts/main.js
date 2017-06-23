bc36d92653ad4dd3303a1220c15703647905f246
Dim And <dimand81179@gmail.com>
2017-06-23 18:03:57 +0300

    Fix for Company page

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -101,6 +101,13 @@ function scrollTo() {
 		}
 		return false;
 	});
+	$('.map-link').click( function(){
+		var scroll_el = $(this).attr('href');
+		if ($(scroll_el).length != 0) {
+			$('html, body').animate({ scrollTop: $(scroll_el).offset().top }, 500);
+		}
+		return false;
+	});
 }
 
 /* scroll to top link */
